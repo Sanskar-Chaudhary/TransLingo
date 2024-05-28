dcf65a72acbd4935a645469c046bcf8641046d17
Sanskar-Chaudhary <sanskarchaudhary2003@gmail.com>
2024-05-28 05:47:27 +0300

    Add box style look to home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -26,97 +26,107 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
       ),
       extendBodyBehindAppBar: true,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF1C1C3A), Color(0xFF2E2E48)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF1C1C3A), Color(0xFF2E2E48)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 80), // For spacing from AppBar
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF2E2E48),
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.white),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: DropdownButton<String>(
-                      value: _fromLanguage,
-                      items: translationController.languages.entries
-                          .map((entry) => DropdownMenuItem<String>(
-                        value: entry.key,
-                        child: Text(entry.value, style: TextStyle(color: Colors.white)),
-                      ))
-                          .toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          _fromLanguage = value!;
-                        });
-                      },
-                      dropdownColor: Color(0xFF2E2E48),
-                      underline: Container(),
-                      isExpanded: true,
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.swap_horiz, color: Colors.green),
-                    onPressed: () {
-                      setState(() {
-                        final temp = _fromLanguage;
-                        _fromLanguage = _toLanguage;
-                        _toLanguage = temp;
-                      });
-                    },
-                  ),
-                  Expanded(
-                    child: DropdownButton<String>(
-                      value: _toLanguage,
-                      items: translationController.languages.entries
-                          .map((entry) => DropdownMenuItem<String>(
-                        value: entry.key,
-                        child: Text(entry.value, style: TextStyle(color: Colors.white)),
-                      ))
-                          .toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          _toLanguage = value!;
-                        });
-                      },
-                      dropdownColor: Color(0xFF2E2E48),
-                      underline: Container(),
-                      isExpanded: true,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                labelText: 'Enter text',
-                labelStyle: TextStyle(color: Colors.white),
-                errorText: _error.isNotEmpty ? _error : null,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 80), // For spacing from AppBar
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF2E2E48),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: DropdownButton<String>(
+                            value: _fromLanguage,
+                            items: translationController.languages.entries
+                                .map((entry) => DropdownMenuItem<String>(
+                              value: entry.key,
+                              child: Text(entry.value, style: TextStyle(color: Colors.white)),
+                            ))
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                _fromLanguage = value!;
+                              });
+                            },
+                            dropdownColor: Color(0xFF2E2E48),
+                            underline: Container(),
+                            isExpanded: true,
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.swap_horiz, color: Colors.green),
+                          onPressed: () {
+                            setState(() {
+                              final temp = _fromLanguage;
+                              _fromLanguage = _toLanguage;
+                              _toLanguage = temp;
+                            });
+                          },
+                        ),
+                        Expanded(
+                          child: DropdownButton<String>(
+                            value: _toLanguage,
+                            items: translationController.languages.entries
+                                .map((entry) => DropdownMenuItem<String>(
+                              value: entry.key,
+                              child: Text(entry.value, style: TextStyle(color: Colors.white)),
+                            ))
+                                .toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                _toLanguage = value!;
+                              });
+                            },
+                            dropdownColor: Color(0xFF2E2E48),
+                            underline: Container(),
+                            isExpanded: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              style: TextStyle(color: Colors.white),
-            ),
-            SizedBox(height: 20),
-            if (_isLoading)
-              CircularProgressIndicator()
-            else
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF2E2E48),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white),
+                ),
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _textController,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    hintText: 'Enter text',
+                    hintStyle: TextStyle(color: Colors.white54),
+                    border: InputBorder.none,
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
                   setState(() {
@@ -137,27 +147,34 @@ class _HomePageState extends State<HomePage> {
                       _isLoading = false;
                     });
                   }
+                  FocusScope.of(context).unfocus(); // Hide keyboard after translation
                 },
                 child: Text('Translate'),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
                   foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
                 ),
               ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Color(0xFF2E2E48),
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.white),
+              SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2E2E48),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  padding: EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      _translatedText,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
-              child: Text(
-                _translatedText,
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
